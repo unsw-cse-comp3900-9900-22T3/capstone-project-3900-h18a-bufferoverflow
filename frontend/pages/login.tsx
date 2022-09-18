10edd693fe807c551f636fd30abaf2b98ada787b
Sean <seanskg72@gmail.com>
2022-09-18 14:11:22 +1000

    login done

--- frontend/pages/login.tsx
+++ frontend/pages/login.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Card, TextField } from "@mui/material"
+import { Box, Button, Card, Divider, TextField, Typography } from "@mui/material"
 import { useState } from "react"
 import { Template } from "../components/Template"
 
@@ -20,28 +20,46 @@ const Login = () => {
           variant="outlined"
           sx={{
             width: 450,
-            minHeight: 300,
+            minHeight: 400,
             padding: 1,
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
+            borderRadius: 3,
             flexDirection: 'column'
           }}
         >
+          <Typography variant="h5" color='primary' component="h2">
+            Login
+          </Typography>
+          <Divider sx={{ mb: 5, mt: 2, width: 200 }} />
           <TextField
             id="outlined-basic"
+            value={email}
+            onChange={change => setEmail(change.target.value)}
             label="Email"
             variant="outlined"
-            sx={{ marginBottom: 1 }}
+            sx={{ marginBottom: 1, width: 250 }}
           />
           <TextField
+            value={password}
+            onChange={change => setPassword(change.target.value)}
             id="outlined-basic"
             label="Password"
             variant="outlined"
             type='password'
-            sx={{ marginBottom: 1 }}
+            sx={{ marginBottom: 3, width: 250 }}
           />
-          <Button variant="outlined">Outlined</Button>
+          <Button
+            variant="outlined"
+            sx={{ width: 250, mb: 2 }}
+            onClick={() => {
+              setEmail('')
+              setPassword('')
+            }}
+          >
+            Login
+          </Button>
         </Card>
       </Box>
     </Template >
