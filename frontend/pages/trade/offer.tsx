3fa35674c5921d369b34603deca0b8e8f502bdc7
Sean <seanskg72@gmail.com>
2022-10-05 18:29:50 +1100

    add trade route

---
+++ frontend/pages/trade/offer.tsx
@@ -0,0 +1,13 @@
+
+import { Template } from "../../components/generic/Template";
+import { NextPage } from "next";
+
+const Offer: NextPage = () => {
+  return (
+    <Template title="Offer">
+
+    </Template>
+  );
+};
+
+export default Offer
---
+++ frontend/pages/trade/propose.tsx
@@ -0,0 +1,13 @@
+
+import { Template } from "../../components/generic/Template";
+import { NextPage } from "next";
+
+const Propose: NextPage = () => {
+  return (
+    <Template title="Propose">
+
+    </Template>
+  );
+};
+
+export default Propose
