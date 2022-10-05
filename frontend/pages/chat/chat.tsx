afe44d89c4a65edfeeb392aa040efb3486c86a18
Sean <seanskg72@gmail.com>
2022-10-05 18:42:09 +1100

    add chat rotue

---
+++ frontend/pages/chat/chat.tsx
@@ -0,0 +1,13 @@
+
+import { Template } from "../../components/generic/Template";
+import { NextPage } from "next";
+
+const Chat: NextPage = () => {
+  return (
+    <Template title="Chat">
+
+    </Template>
+  );
+};
+
+export default Chat
---
+++ frontend/pages/chat/overview.tsx
@@ -0,0 +1,13 @@
+
+import { Template } from "../../components/generic/Template";
+import { NextPage } from "next";
+
+const ChatOverview: NextPage = () => {
+  return (
+    <Template title="Chat Overview">
+
+    </Template>
+  );
+};
+
+export default ChatOverview
