8a5b06d7ecf2343aafce9c86f7a5fc672f4d253d
Sean <seanskg72@gmail.com>
2022-10-05 18:50:58 +1100

    add detailed listing

---
+++ frontend/pages/listing/have.tsx
@@ -0,0 +1,13 @@
+
+import { Template } from "../../components/generic/Template";
+import { NextPage } from "next";
+
+const HaveListing: NextPage = () => {
+  return (
+    <Template title="Have Listing">
+
+    </Template>
+  );
+};
+
+export default HaveListing
---
+++ frontend/pages/listing/want.tsx
@@ -0,0 +1,13 @@
+
+import { Template } from "../../components/generic/Template";
+import { NextPage } from "next";
+
+const WantListing: NextPage = () => {
+  return (
+    <Template title="Want Listing">
+
+    </Template>
+  );
+};
+
+export default WantListing
