8e0647a6ef6c32510242b83c9b569bdc40d094d6
sschreyer <samschreyer360@gmail.com>
2022-10-07 20:32:57 +1100

    Add user table to backend database

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -9,12 +9,21 @@ from flask_sqlalchemy import SQLAlchemy
 app.config.from_object("app.config.Config")
 db = SQLAlchemy(app)
 
+class Address(db.Model):
+    __tablename__ = "addresses"
+
+    id = db.Column(db.Integer, primary_key=True)
+
 class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(128), unique=True, nullable=False)
     active = db.Column(db.Boolean(), default=True, nullable=False)
+    preferredDistance = db.Column(db.Integer, default=100, nullable=False)
+    bio = db.Column(db.String(500), default="", nullable=False)
+    displayImg = db.Column(db.String(128), default=None, nullable=True)
+    address = db.Column(db.Integer, db.ForeignKey("addresses.id"), nullable=False)
 
     def __init__(self, email):
         self.email = email
