4d4532527a663beb0e3d485e587348cb4ea03339
sschreyer <samschreyer360@gmail.com>
2022-10-08 19:06:09 +1100

    Fix GraphQL fetch user data not working for certain fields

--- backend/app/models.py
+++ backend/app/models.py
@@ -9,17 +9,23 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
-    # email = db.Column(db.String(128), unique=True, nullable=False)
-    # active = db.Column(db.Boolean(), default=True, nullable=False)
-    # preferred_distance = db.Column(db.Integer, default=100, nullable=False)
-    # bio = db.Column(db.String(500), default="", nullable=False)
-    # display_img = db.Column(db.String(128), default=None, nullable=True)
-    # address = db.Column(db.Integer, db.ForeignKey("addresses.id"), nullable=True)
+    email = db.Column(db.String(128), unique=True, nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+    preferred_distance = db.Column(db.Integer, default=100, nullable=False)
+    bio = db.Column(db.String(500), default="", nullable=False)
+    display_img = db.Column(db.String(128), default=None, nullable=True)
+    address = db.Column(db.Integer, db.ForeignKey("addresses.id"), nullable=True)
 
-    # def __init__(self, email):
-    #     self.email = email
+    def __init__(self, email):
+        self.email = email
 
     def to_dict(self):
         return {
-            "id": self.id
+            "id": self.id,
+            "email": self.email, 
+            "active": self.active,
+            "preferred_distance": self.preferred_distance,
+            "bio": self.bio, 
+            "display_img": self.display_img,
+            "address": self.address
         }
