546f8d49e649c0e2a06d277c6d6cdcc36d2d0cb1
trollic <cece.jzhu@gmail.com>
2022-10-08 16:12:14 +1100

    play with logged in and logged out logic

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,14 +2,16 @@ import { Button } from '@mui/material'
 import type { NextPage } from 'next'
 import { useRouter } from 'next/router'
 import { Template } from '../components/generic/Template'
-import DefaultFeed from '../components/feed/default'
 import { useStore } from '../store/store'
 import RecommendedFeed from '../components/feed/recommended'
+import { Landing } from '../components/landing/Landing'
 
 const Home: NextPage = () => {
   const auth = useStore()
+  const [loggedIn, setLoggedIn] = useStore(auth)
+
   const router = useRouter()
-  return <Template title='Swapr'>{auth ? <RecommendedFeed /> : <DefaultFeed />}</Template>
+  return <Template title='Swapr'>{auth ? <RecommendedFeed /> : <Landing />}</Template>
 }
 
 export default Home
