004765f7412cd546baf9847b602ef80aa4d4f80c
Sean <seanskg72@gmail.com>
2022-10-09 12:05:12 +1100

    update recommended and default page layout

--- frontend/pages/feed/default.tsx
+++ frontend/pages/feed/default.tsx
@@ -1,22 +1,27 @@
 import { Template } from '../../components/generic/Template'
 import { NextPage } from 'next'
 import { ItemCard } from '../../components/feed/ItemCard'
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const DefaultFeed: NextPage = () => {
   return (
     <Template title='Swapr'>
-      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography sx={{ width: '85vw', fontWeight: 'bold', mt: 2, mb: 1 }}>
+          4323 Items for Sale
+        </Typography>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', width: '90vw' }}>
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+        </Box>
       </Box>
     </Template>
   )
--- frontend/pages/feed/recommended.tsx
+++ frontend/pages/feed/recommended.tsx
@@ -1,4 +1,4 @@
-import { Box } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 import { NextPage } from 'next'
 import { ItemCard } from '../../components/feed/ItemCard'
 import { Template } from '../../components/generic/Template'
@@ -6,17 +6,22 @@ import { Template } from '../../components/generic/Template'
 const RecommendedFeed: NextPage = () => {
   return (
     <Template title='Swapr'>
-      <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
-        <ItemCard />
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <Typography sx={{ width: '85vw', fontWeight: 'bold', mt: 2, mb: 1 }}>
+          Recommended Feed
+        </Typography>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', width: '90vw' }}>
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+          <ItemCard />
+        </Box>
       </Box>
     </Template>
   )
