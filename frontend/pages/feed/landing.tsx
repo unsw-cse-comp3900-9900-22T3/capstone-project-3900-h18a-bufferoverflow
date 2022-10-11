ec127cc8dd9682ec6f800538167f960342d9ef78
Jack Robbers <jack@2pisoftware.com>
2022-10-11 15:09:09 +1100

    improve spacing between items

--- frontend/pages/feed/landing.tsx
+++ frontend/pages/feed/landing.tsx
@@ -1,27 +1,31 @@
 import { Template } from "../../components/generic/Template";
 import { NextPage } from "next";
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Stack, Button, Typography } from "@mui/material";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Landing: NextPage = () => {
   return (
     <Template title="Swapr" center>
-      <Box id="landing-box">
-        <h1>Welcome to Swapr</h1>
-        <Typography variant="body1">A platform for trading secondhand items</Typography>
+      <Stack id="landing-box" spacing={2}>
+        <Typography variant="h1">Welcome to Swapr</Typography>
+        <Typography variant="body1">
+          A platform for trading secondhand items
+        </Typography>
         <Typography variant="body1">
           Create listings for something you have - or something you're looking
           for!
         </Typography>
-        <Button
-          sx={{ textTransform: "none" }}
-          href="/feed/default"
-          variant="outlined"
-          endIcon={<ArrowForwardIosIcon />}
-        >
-          Go To Listings
-        </Button>
-      </Box>
+        <Box>
+          <Button
+            sx={{ textTransform: "none" }}
+            href="/feed/default"
+            variant="outlined"
+            endIcon={<ArrowForwardIosIcon />}
+          >
+            Go To Listings
+          </Button>
+        </Box>
+      </Stack>
     </Template>
   );
 };
