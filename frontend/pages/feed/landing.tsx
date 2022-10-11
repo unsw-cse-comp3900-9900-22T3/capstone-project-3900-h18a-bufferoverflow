433e1be26d8ea062e389bfa3493b4e8624ec9504
Jack Robbers <jack@2pisoftware.com>
2022-10-11 14:49:12 +1100

    add Sean's suggestions

--- frontend/pages/feed/landing.tsx
+++ frontend/pages/feed/landing.tsx
@@ -1,21 +1,32 @@
-import { Template } from '../../components/generic/Template'
-import { NextPage } from 'next'
-import { ItemCard } from '../../components/feed/ItemCard'
-import { Box, Button } from '@mui/material'
+import { Template } from "../../components/generic/Template";
+import { NextPage } from "next";
+import { ItemCard } from "../../components/feed/ItemCard";
+import { Box, Button } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Landing: NextPage = () => {
   return (
-    <Template title='Swapr'>
+    <Template title="Swapr" center>
       <Box id="landing-box">
         <Box>
           <h1>Welcome to Swapr</h1>
-          <p>A platform for trading secondhand items</p>
-          <p>Create listings for something you have - or something you're looking for!</p>
-          <Button href='/feed/default' variant="outlined">Check it out here</Button>
+          <p className="grey-body">A platform for trading secondhand items</p>
+          <p className="grey-body">
+            Create listings for something you have - or something you're looking
+            for!
+          </p>
+          <Button
+            sx={{ textTransform: "none" }}
+            href="/feed/default"
+            variant="outlined"
+            endIcon={<ArrowForwardIosIcon/>}
+          >
+            Go To Listings
+          </Button>
         </Box>
       </Box>
     </Template>
-  )
-}
+  );
+};
 
-export default Landing
+export default Landing;
