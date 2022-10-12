c957647956dedb8ff0bca06b915fb931ee80ef4d
Sean <seanskg72@gmail.com>
2022-10-12 18:30:08 +1100

    EZ3900-58 fix object reference issue with mock data

--- frontend/pages/detailed-listing/have.tsx
+++ frontend/pages/detailed-listing/have.tsx
@@ -4,6 +4,7 @@ import { Avatar, Box, Button, Card, Typography } from "@mui/material";
 import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import PersonIcon from '@mui/icons-material/Person';
 import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+import { useRouter } from "next/router";
 
 /////////////////////////////////////////////////////////////////////////////
 // Data Types
@@ -64,6 +65,10 @@ const DescriptionBox = (props: {
 
 const DetailedHaveListing: NextPage = () => {
 
+  // Get item name from query params
+  const router = useRouter()
+  const { title } = router.query
+
   const image = 'https://images.unsplash.com/photo-1499720565725-bd574541a3ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80'
   const categories = [
     "asdfsadf",
@@ -106,7 +111,7 @@ const DetailedHaveListing: NextPage = () => {
                 : <></>
             }
           </Card>
-          <Typography sx={{ p: 3, pl: 4, fontSize: 20, fontWeight: 'bold' }}>Item Name</Typography>
+          <Typography sx={{ p: 3, pl: 4, fontSize: 20, fontWeight: 'bold' }}>{title}</Typography>
           <DescriptionBox icon={<AttachMoneyIcon />} description={purchaseOptions} />
           <DescriptionBox icon={<PersonIcon />} description={itemPosessor} />
           <DescriptionBox icon={<LocalShippingIcon />} description={shippingOptions} />
--- frontend/utils/mockdata.ts
+++ frontend/utils/mockdata.ts
@@ -5,14 +5,14 @@ export const mockRequest = async (): Promise<ItemCardProps[]> => {
   await new Promise(resolve => setTimeout(resolve, 1000));
   let data = []
   for (let i = 0; i < 20; i++) {
-    data.push({
+    data.push(structuredClone({
       title: 'Used Kayak',
       price: 9999,
       location: 'Kensingston, NSW',
       image: 'https://images.unsplash.com/photo-1499720565725-bd574541a3ee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
       avatar: 'https://mui.com/static/images/avatar/3.jpg',
-      want: !!(i % 3 == 0)
-    })
+      want: i % 3 === 0
+    }))
   }
   return data
 }
