6ba065bd48e286ba0a5ddd61941181a8c2295ee6
trollic <cece.jzhu@gmail.com>
2022-10-17 14:41:53 +1100

    EZ3900-91/chat overview basic stack structure created

--- frontend/pages/chat/overview.tsx
+++ frontend/pages/chat/overview.tsx
@@ -1,15 +1,26 @@
 
 import { Template } from "../../components/generic/Template";
 import { NextPage } from "next";
+import { Stack } from "@mui/system";
 import { SingleChatOverview } from "../../components/chat/single-chat";
 
 const ChatOverview: NextPage = () => {
   return (
     <Template title="Chat Overview">
-      <SingleChatOverview />
-      <SingleChatOverview />
-      <SingleChatOverview />
-      <SingleChatOverview />
+      <h3>Active Chats</h3>
+      <Stack spacing={2}>
+        <SingleChatOverview />
+        <SingleChatOverview />
+        <SingleChatOverview />
+        <SingleChatOverview />
+      </Stack>
+      <h3>Inactive Chats</h3>
+      <Stack spacing={2}>
+        <SingleChatOverview />
+        <SingleChatOverview />
+        <SingleChatOverview />
+        <SingleChatOverview />
+      </Stack>
     </Template>
   );
 };
