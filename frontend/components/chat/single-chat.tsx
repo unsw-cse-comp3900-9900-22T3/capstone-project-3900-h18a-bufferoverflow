f37de0cbc43de7e029783c0330861b883b90e264
trollic <cece.jzhu@gmail.com>
2022-10-17 15:10:50 +1100

    EZ3900-91/add props for dynamic handling of username and time for each chat

--- frontend/components/chat/single-chat.tsx
+++ frontend/components/chat/single-chat.tsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import { Avatar , IconButton} from "@mui/material"
+import { Box, Stack } from "@mui/system"
+import DeleteForever from "@mui/icons-material/DeleteOutline"
 
-export const SingleChatOverview = (props: {
-}) => {
+export const SingleChatOverview = (props: { 
+        href: string;
+        username: string;
+        avatar: string;
+        lastMessageTime: string; 
+    }) => {
   return (
-    <div>This is a single chat</div> 
-  )
-}
+    <Stack direction="row" spacing={2}>
+      <Avatar src={props.avatar}/>
+      <text>{props.username}</text>
+      <text>{props.lastMessageTime}</text>
+      <IconButton
+        size="large"
+        aria-label="go to chat with this user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        href={props.href}
+        sx={{}}
+      >
+        <DeleteForever/>
+      </IconButton>
+    </Stack>
+  );
+};
