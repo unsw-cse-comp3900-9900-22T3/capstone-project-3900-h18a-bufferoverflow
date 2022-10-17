0f35fe4ee11e9958fc11aac935e66e00a2b76b77
Jack Robbers <jack@2pisoftware.com>
2022-10-17 13:39:40 +1100

    EZ3900-47: add flask-socketio

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
 CORS(app)
 app.config.from_object("app.config.Config")
 db = SQLAlchemy(app)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+socketio.run(app)
