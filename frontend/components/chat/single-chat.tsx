83226a890ef8753021144df2c71cc0e99a9a3ee0
trollic <cece.jzhu@gmail.com>
2022-10-17 16:08:55 +1100

    EZ3900-91/single chat separated delete button styling

--- frontend/components/chat/single-chat.tsx
+++ frontend/components/chat/single-chat.tsx
@@ -1,5 +1,5 @@
 import { Avatar , IconButton } from "@mui/material"
-import { Stack } from "@mui/system"
+import { Box, Stack } from "@mui/system"
 import DeleteForever from "@mui/icons-material/DeleteOutline"
 
 export interface SingleChatOverviewProps {
@@ -16,10 +16,20 @@ export const SingleChatOverview = (props: {
         lastMessageTime: string; 
 }) => {
   return (
-    <Stack direction="row" spacing={2}>
-      <Avatar src={props.avatar}/>
-      <text>{props.username}</text>
-      <text>{props.lastMessageTime}</text>
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+        border: "1px solid #e6e6e6",
+      }}
+    >
+      <Stack direction="row" spacing={2}>
+        <Avatar src={props.avatar} />
+        <text>{props.username}</text>
+        <text>{props.lastMessageTime}</text>
+      </Stack>
       <IconButton
         size="large"
         aria-label="go to chat with this user"
@@ -28,8 +38,8 @@ export const SingleChatOverview = (props: {
         href={props.href}
         sx={{}}
       >
-        <DeleteForever/>
+        <DeleteForever />
       </IconButton>
-    </Stack>
+    </Box>
   );
 };
