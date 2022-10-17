cdb0fb526acb21594fed857ee306de7f39583969
trollic <cece.jzhu@gmail.com>
2022-10-17 14:27:38 +1100

    EZ3900-44/skeleton component for single item in chat overview

--- frontend/pages/chat/overview.tsx
+++ frontend/pages/chat/overview.tsx
@@ -1,11 +1,15 @@
 
 import { Template } from "../../components/generic/Template";
 import { NextPage } from "next";
+import { SingleChatOverview } from "../../components/chat/single-chat";
 
 const ChatOverview: NextPage = () => {
   return (
     <Template title="Chat Overview">
-
+      <SingleChatOverview />
+      <SingleChatOverview />
+      <SingleChatOverview />
+      <SingleChatOverview />
     </Template>
   );
 };
