927242891d78d6edf0f6719bf42a7a0b04a922bc
Sean <seanskg72@gmail.com>
2022-10-18 20:34:29 +1100

    EZ3900-24 create template

---
+++ frontend/components/listing/ListingTemplate.tsx
@@ -0,0 +1,152 @@
+import { Box, Button, Card, Checkbox, FormControl, FormControlLabel, FormGroup, InputAdornment, Tab, Tabs, TextField, Typography } from "@mui/material";
+import { createRef, useState } from "react";
+import { ListingProps, StatusType } from "../../components/listing/types";
+import { uploadFile } from "../../utils/imageUtils";
+import { CategorySearch } from "../../components/feed/CategorySearch";
+
+/////////////////////////////////////////////////////////////////////////////
+// Secondary Components
+/////////////////////////////////////////////////////////////////////////////
+
+const Slider = (props: {
+  status: StatusType;
+  setStatus: (arg: StatusType) => void;
+}) => {
+  return (
+    <Box sx={{ display: 'flex', alignItems: 'center', mt: 0.5, ml: 0.5 }}>
+      <Typography sx={{ fontSize: 16, fontWeight: 'bold', mr: 3, mt: 0.9 }}>Status</Typography>
+      <Tabs
+        value={props.status}
+        onChange={(_, val) => props.setStatus(val)}
+        textColor='secondary'
+        indicatorColor="secondary"
+        sx={{ mt: 1.5 }}
+      >
+        <Tab label="available" value={'available'} />
+        <Tab label="pending" value={'pending'} />
+      </Tabs>
+    </Box>
+  )
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// Primary Component
+/////////////////////////////////////////////////////////////////////////////
+
+export const ListingTemplate = (props: {
+  data?: ListingProps;
+}) => {
+
+  const ref = createRef<any>()
+
+  const [title, setTitle] = useState<string>('')
+  const [image, setImage] = useState<string>('')
+  const [description, setDescription] = useState<string>('')
+  const [location, setLocation] = useState<string>('')
+  const [categories, setCategories] = useState<string[]>([])
+  const [status, setStatus] = useState<StatusType>('available')
+  const [trade, setTrade] = useState<boolean>(false)
+  const [cash, setCash] = useState<boolean>(false)
+  const [bank, setBank] = useState<boolean>(false)
+  const [tradeDescription, setTradeDescription] = useState<string>('')
+  const [weight, setWeight] = useState<number>()
+  const [volume, setVolume] = useState<number>()
+  const [materials, setMaterials] = useState<string[]>([])
+  const [tradeCategories, setTradeCategories] = useState<string[]>([])
+  const [price, setPrice] = useState<number>(0)
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 40 }}>
+
+      {/** Left Section */}
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <Typography sx={{ fontSize: 20, fontWeight: 'bold', mb: 2 }}>Item Information</Typography>
+        <TextField value={title} label="Title" variant="outlined" sx={{ mb: 1 }} onChange={e => setTitle(e.target.value)} />
+        <Card variant="outlined" sx={{ height: 280, width: 450, borderRadius: 4 }}>
+          {
+            image
+              ? <img src={image} alt='profile' style={{ height: '100%', width: '100%' }} />
+              : <></>
+          }
+        </Card>
+        <Button component="label" sx={{ mb: 1, mt: 1 }}>
+          Select Photo
+          <input id='bob' ref={ref} type="file" hidden accept='image/png, image/jpeg' onChange={async () => {
+            if (ref.current.files[0]) setImage(await uploadFile(ref.current.files[0]))
+          }} />
+        </Button>
+        <TextField value={description} label="Description" variant="outlined" sx={{ mb: 1.5 }} multiline rows={3} onChange={e => setDescription(e.target.value)} />
+        <Typography sx={{ fontSize: 16, fontWeight: 'bold', mb: 1.5, ml: 0.5 }}>Location</Typography>
+        <TextField value={location} label="Location" variant="outlined" sx={{ mb: 1.5 }} onChange={e => setLocation(e.target.value)} />
+        <CategorySearch
+          setCategories={setCategories}
+          validCategories={['Entertainment', 'Vehicles']}
+          title="Category"
+          width={450}
+        />
+        <Slider status={status} setStatus={setStatus} />
+      </Box>
+
+      {/** Right Section */}
+      <Box sx={{ display: 'flex', flexDirection: 'column', width: 450 }}>
+        <Typography sx={{ fontSize: 18, fontWeight: 'bold', mb: 2 }}>What I'm Looking For In Return</Typography>
+        <Typography sx={{ fontSize: 16, mb: 0.5, ml: 0.5 }}>Accepted Trade / Payment Methods</Typography>
+        <FormControl sx={{ m: 0.5, mb: 1.5 }} component="fieldset" variant="standard">
+          <FormGroup>
+            <FormControlLabel
+              control={<Checkbox checked={trade} onChange={() => setTrade(!trade)} />}
+              label="Trade for another item"
+            />
+            <FormControlLabel
+              control={<Checkbox checked={cash} onChange={() => setCash(!cash)} />}
+              label="Payment in cash"
+            />
+            <FormControlLabel
+              control={<Checkbox checked={bank} onChange={() => setBank(!bank)} />}
+              label="Payment by bank transfer or payment service"
+            />
+          </FormGroup>
+        </FormControl>
+        <TextField value={tradeDescription} label="What I want as a trade" variant="outlined" sx={{ mb: 1.5 }} multiline rows={3} onChange={e => setTradeDescription(e.target.value)} />
+        <Typography sx={{ fontSize: 16, fontWeight: 'bold', mb: 1.5, ml: 0.5 }}>Approximate Item Stats</Typography>
+        <Box sx={{ width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'space-between', mb: 1.5 }}>
+          <TextField value={weight} type='number' label="Weight (kg)" variant="outlined" onChange={e => setWeight(parseInt(e.target.value))} />
+          <Typography sx={{ fontSize: 16 }}>or</Typography>
+          <TextField value={volume} type='number' label="Volume (cubic m)" variant="outlined" onChange={e => setVolume(parseInt(e.target.value))} />
+        </Box>
+        <Typography sx={{ fontSize: 16, fontWeight: 'bold', mb: 1.5, ml: 0.5 }}>Materials</Typography>
+        <CategorySearch
+          setCategories={setMaterials}
+          validCategories={['Wood', 'Metal']}
+          title="Materials"
+          width={'100%'}
+        />
+        <Typography sx={{ fontSize: 16, fontWeight: 'bold', mb: 1.5, ml: 0.5, mt: 1.5 }}>Willing To Trade For Categories</Typography>
+        <CategorySearch
+          setCategories={setMaterials}
+          validCategories={['Wood', 'Metal']}
+          title="Materials"
+          width={'100%'}
+        />
+        <Box sx={{ display: 'flex', alignItems: 'center', mt: 2.5, mb: 2.5 }}>
+          <Typography sx={{ fontSize: 16, fontWeight: 'bold', mr: 2.5, ml: 0.5 }}>Price</Typography>
+          <TextField
+            value={price}
+            type='number'
+            label="Price"
+            variant="outlined"
+            onChange={e => setPrice(parseInt(e.target.value))}
+            sx={{ width: '100%' }}
+            InputProps={{
+              startAdornment: <InputAdornment position="start">$</InputAdornment>
+            }}
+          />
+        </Box>
+        <Button variant="outlined" sx={{ borderRadius: 30, width: '100%', height: 45 }}>
+          Post Have Listing
+        </Button>
+      </Box>
+
+    </Box >
+  )
+}
