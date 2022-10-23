40770367fbf1db8e00c0c997a1e407323f28a5ab
Jack Robbers <jack@2pisoftware.com>
2022-10-23 12:53:16 +1100

    chat: initial client setup

--- frontend/pages/chat/chat.tsx
+++ frontend/pages/chat/chat.tsx
@@ -1,22 +1,11 @@
-
 import { Template } from "../../components/generic/Template";
 import { NextPage } from "next";
-import { useEffect } from 'react'
-import io from 'socket.io-client'
+import { useEffect } from "react";
+import { io } from "socket.io-client";
 import { Typography } from "@mui/material";
-let socket
+let socket;
 
 const Chat: NextPage = () => {
-  useEffect(() => socketInitializer(), [])
-
-  const socketInitializer = async () => {
-    await fetch('/api/socket')
-    socket = io()
-
-    socket.on('connect', () => {
-      console.log('connected')
-    })
-  }
 
   return (
     <Template title="Chat">
@@ -25,4 +14,4 @@ const Chat: NextPage = () => {
   );
 };
 
-export default Chat
+export default Chat;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from 'next'
+import { useEffect} from 'react'
+import { io } from "socket.io-client";
+let socket
+
 import { useStore } from '../store/store'
 import Landing from './feed/landing';
 import RecommendedFeed from './feed/recommended'
 
 const Home: NextPage = () => {
   const { auth } = useStore();
+
+  useEffect(() => socketInitializer(), []);
+
+  const socketInitializer = async () => {
+    await fetch("/api/socket");
+    socket = io("");
+    
+    socket.on("connect", () => {
+      console.log("connected");
+    });
+  };
+
+
   return (<>{auth ? <RecommendedFeed /> : <Landing />}</>)
 }
 
