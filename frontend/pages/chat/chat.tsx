9200947c4d814080bde050a3c3a0a8d887b8a31f
Jack Robbers <jack@2pisoftware.com>
2022-10-23 12:37:38 +1100

    chat: add socketio client

--- frontend/pages/chat/chat.tsx
+++ frontend/pages/chat/chat.tsx
@@ -1,11 +1,26 @@
 
 import { Template } from "../../components/generic/Template";
 import { NextPage } from "next";
+import { useEffect } from 'react'
+import io from 'socket.io-client'
+import { Typography } from "@mui/material";
+let socket
 
 const Chat: NextPage = () => {
+  useEffect(() => socketInitializer(), [])
+
+  const socketInitializer = async () => {
+    await fetch('/api/socket')
+    socket = io()
+
+    socket.on('connect', () => {
+      console.log('connected')
+    })
+  }
+
   return (
     <Template title="Chat">
-
+      <Typography>Hello</Typography>
     </Template>
   );
 };
