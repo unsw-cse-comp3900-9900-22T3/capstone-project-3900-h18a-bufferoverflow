815b9495ce5ca567cb2f9b5be6b59a2d48774fba
sschreyer <samschreyer360@gmail.com>
2022-10-28 18:16:26 +1100

    EZ3900-90/listing-fetch-api: fix getListing; fix database seeding

--- backend/server.py
+++ backend/server.py
@@ -7,7 +7,8 @@ from app.userQueries import listUsers_resolver, getUser_resolver, \
     create_user_resolver, update_user_resolver, delete_user_resolver
 from app.listingQueries import defaultFeed_resolver, create_listing_resolver, \
     update_listing_resolver, delete_listing_resolver, userFeed_resolver, \
-    searchListings_resolver, getCategories_resolver, getMaterials_resolver
+    searchListings_resolver, getCategories_resolver, getMaterials_resolver, \
+    getListing_resolver
 from app.models import User
 
 
@@ -15,6 +16,7 @@ from app.models import User
 query = ObjectType("Query")
 query.set_field("listUsers", listUsers_resolver)
 query.set_field("getUser", getUser_resolver)
+query.set_field("getListing", getListing_resolver)
 query.set_field("defaultFeed", defaultFeed_resolver)
 query.set_field("userFeed", userFeed_resolver)
 query.set_field("searchListings", searchListings_resolver)
