c0d05663fd0cc50867c1c74c526c05a1dad389af
Jack Robbers <jack@2pisoftware.com>
2022-10-31 13:14:20 +1100

    chat: make cors port not hardcoded

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -9,7 +10,9 @@ app.config.from_object("app.config.Config")
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = 'secret!'
 
-socketio = SocketIO(app, cors_allowed_origins=['http://localhost:5002'])
+print(os.environ)
+print(f'hello - http://localhost:{os.environ["NEXT_PUBLIC_FRONTEND_PORT"]}')
+socketio = SocketIO(app, cors_allowed_origins=[f'http://localhost:{os.environ["NEXT_PUBLIC_FRONTEND_PORT"]}'])
 if __name__ == '__main__':
     socketio.run(app)
 
