003e1cca5a8cc0482cfd086d3325d9d80990e1ba
sschreyer <samschreyer360@gmail.com>
2022-11-08 22:42:25 +1100

    0EZ3900-101/write-data-gen-script: Further skeleton setup for data gen

--- backend/datagen.py
+++ backend/datagen.py
@@ -6,17 +6,34 @@ X_TRADES = 5
 Y_SEARCHES = 20 
 Z_CLICKS = 10
 
+
 # generate n trades 
-def gen_trades(n_trades):
-    pass
+def gen_trades(n_trades, trades=[]):
+    if n_trades == 0:
+        return trades 
+
+    # TODO: implement trade generation algorithm
+
+    gen_trades(n_trades - 1, trades)
+
 
 # generate n searches 
-def gen_searches(n_searches):
-    pass 
+def gen_searches(n_searches, searches=[]):
+    if n_searches == 0:
+        return searches 
+
+    # TODO: implement search generation algorithm
+
+    gen_searches(n_searches - 1, searches) 
 
 # generate n clicks 
-def gen_clicks(n_clicks):
-    pass
+def gen_clicks(n_clicks, clicks=[]):
+    if n_clicks == 0:
+        return clicks 
+
+    # TODO: implement clicks generation algorithm
+
+    gen_clicks(n_clicks - 1, clicks)
 
 def gen_data():
     # generate data for N users 
