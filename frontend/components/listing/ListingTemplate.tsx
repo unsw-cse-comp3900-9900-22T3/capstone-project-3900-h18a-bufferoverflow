af53c2e17ec3027c3c0f6c7a62aace2dcbf16ae4
trollic <cece.jzhu@gmail.com>
2022-11-09 14:57:27 +1100

    EZ3900-49/updated getListing call with userEmail

--- frontend/components/listing/ListingTemplate.tsx
+++ frontend/components/listing/ListingTemplate.tsx
@@ -56,8 +56,8 @@ const CREATE_LISTING = gql`
 `
 
 const GET_LISTING = gql`
-  query ($id: ID!) {
-    getListing(id: $id) {
+  query ($id: ID!, $userEmail: String) {
+    getListing(id: $id, userEmail: $userEmail) {
       listing {
         id
         title
@@ -225,7 +225,7 @@ export const ListingTemplate = (props: {
 
   useEffect(() => {
     if (id) {
-      execQuery({ variables: { id } });
+      execQuery({ variables: { id : id, userEmail: auth?.email } });
     }
     if (data && data?.getListing.listing) {
       setTitle(data.getListing.listing.title);
