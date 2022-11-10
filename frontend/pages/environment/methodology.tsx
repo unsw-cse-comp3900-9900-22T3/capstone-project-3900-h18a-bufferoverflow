3997f0bd4ba62e50fbae6c35a3571dac205c3214
Luke Banicevic <60857954+banaboi@users.noreply.github.com>
2022-11-10 18:52:37 +1100

    EZ3900-105/stats-method: addressing review comments

--- frontend/pages/environment/methodology.tsx
+++ frontend/pages/environment/methodology.tsx
@@ -7,12 +7,12 @@ const Methodology: NextPage = () => {
     <Template title="Methodology">
       <h1>How do we estimate these stats?</h1>
       <p>
-        Our carbon emission estimates are based on the combined saving of brick and mortar delivery services,
-        manufacturing and accumilating landfill waste if you were to trade away your unwanted items for other items you want.
+        Our carbon emission estimates are based on the combined saving of delivery,
+        manufacturing and avoided landfill waste by trading.
         There are some small assumptions we have to make in our calculations. This is reviewed by a panel of experts in the field of climate change and sustainability.
         These include:
         <ul>
-          <li>Brick and Mortar deliveries are performed by light commercial vehicles which run on E10 ethanol</li>
+          <li>Deliveries are performed by light commercial vehicles which run on E10 ethanol</li>
           <li>Light commercial vehicles use 12.8L of fuel per 100km, and travel 52,307,000km in a year on average </li>
           <li>Light commercial vehicles total litres of ethanol burnt yearly equates to 6,681,360L</li>
           <li>Light commercial vehicles emit 1.5kg of CO2 per litre of ethanol burnt, equating to 78,460,800kg of CO2 per year</li>
@@ -38,7 +38,7 @@ const Methodology: NextPage = () => {
             <p>
               Manufacturing new items emits CO2 proportional to the mass of the item, and 35% of excess material which is not used to create the product. The amount of CO2 emitted is determined by the materials used in the manufacturing process.
             </p>
-          <li>Brick and Mortar Delivery</li>
+          <li>Delivery</li>
           <p>
             When ever you order an item online, it is often delivered to you by a light commercial vehicle. There are approximately 500,000 deliveries in Australia per year. The amount of CO2 emitted from these deliveries is calculated using the emission factors listed above.
           </p>
